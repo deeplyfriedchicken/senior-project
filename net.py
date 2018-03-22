fec8be701f0271a2ffea03dd140015829c4dc980
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2018-03-21 22:06:54 -0700

    updated neural net after thesis meetings

--- net.py
+++ net.py
@@ -6,6 +6,7 @@ import re
 from random import randint
 import datetime
 import time
+from sklearn.utils import shuffle
 
 # Removes punctuation, parentheses, question marks, etc., and leaves only alphanumeric characters
 strip_special_chars = re.compile("[^A-Za-z0-9 ]+")
@@ -13,13 +14,6 @@ strip_special_chars = re.compile("[^A-Za-z0-9 ]+")
 def cleanSentences(string):
     string = string.lower().replace("<br />", " ")
     return re.sub(strip_special_chars, "", string.lower())
-
-def unison_shuffled_copies(a, b):
-    assert len(a) == len(b)
-    p = np.random.permutation(len(a))
-    print(len(p))
-    print(p)
-    return a[p], b[p]
 
 maxSeqLength = 25 #Maximum length of sentence
 numDimensions = 300 #Dimensions for each word vector
@@ -38,7 +32,8 @@ for pf in positiveFiles:
         counter = len(line.split())
         numWords.append(counter)       
 print('Positive files finished')
-print(len(positiveFiles))
+
+minLength = len(positiveFiles)
 
 for nf in negativeFiles:
     with open(nf, "r", encoding='utf-8') as f:
@@ -46,84 +41,105 @@ for nf in negativeFiles:
         counter = len(line.split())
         numWords.append(counter)  
 print('Negative files finished')
-print(len(negativeFiles))
+
+if (minLength > len(negativeFiles)):
+    minLength = len(negativeFiles)
 
 numFiles = len(numWords)
 print('The total number of files is', numFiles)
 print('The total number of words in the files is', sum(numWords))
 print('The average number of words in the files is', sum(numWords)/len(numWords))
 
-ids = np.zeros((numFiles, maxSeqLength), dtype='int32')
+ids = np.zeros((minLength*2, maxSeqLength), dtype='int32')
 labels = []
 fileCounter = 0
+counter = 0
 for pf in positiveFiles:
-   with open(pf, "r") as f:
-       indexCounter = 0
-       line=f.readline()
-       cleanedLine = cleanSentences(line)
-       split = cleanedLine.split()
-       for word in split:
-           try:
-               ids[fileCounter][indexCounter] = wordsList.index(word)
-           except ValueError:
-               ids[fileCounter][indexCounter] = 399999 #Vector for unkown words
-           indexCounter = indexCounter + 1
-           if indexCounter >= maxSeqLength:
-               break
-       fileCounter = fileCounter + 1
-       labels.append([1,0])
+    if (counter >= minLength):
+        break
+    with open(pf, "r") as f:
+        indexCounter = 0
+        line=f.readline()
+        cleanedLine = cleanSentences(line)
+        split = cleanedLine.split()
+        for word in split:
+            try:
+                ids[fileCounter][indexCounter] = wordsList.index(word)
+            except ValueError:
+                ids[fileCounter][indexCounter] = 399999 #Vector for unkown words
+            indexCounter = indexCounter + 1
+            if indexCounter >= maxSeqLength:
+                break
+        labels.append([1,0])
+        fileCounter = fileCounter + 1
+        counter = counter + 1
 
+counter = 0
 for nf in negativeFiles:
-   with open(nf, "r") as f:
-       indexCounter = 0
-       line=f.readline()
-       cleanedLine = cleanSentences(line)
-       split = cleanedLine.split()
-       for word in split:
-           try:
-               ids[fileCounter][indexCounter] = wordsList.index(word)
-           except ValueError:
-               ids[fileCounter][indexCounter] = 399999 #Vector for unkown words
-           indexCounter = indexCounter + 1
-           if indexCounter >= maxSeqLength:
-               break
-       fileCounter = fileCounter + 1 
-       labels.append([0,1])
+    if (counter > minLength):
+        break
+    with open(nf, "r") as f:
+        indexCounter = 0
+        line=f.readline()
+        cleanedLine = cleanSentences(line)
+        split = cleanedLine.split()
+        for word in split:
+            try:
+                ids[fileCounter][indexCounter] = wordsList.index(word)
+            except ValueError:
+                ids[fileCounter][indexCounter] = 399999 #Vector for unkown words
+            indexCounter = indexCounter + 1
+            if indexCounter >= maxSeqLength:
+                break
+        labels.append([0,1])
+        fileCounter = fileCounter + 1 
+        counter = counter + 1
 #Pass into embedding function and see if it evaluates. 
 
+labels = np.asarray(labels)
 np.save('idsMatrix', ids)
+np.save('labels', labels)
 
 ids = np.load('idsMatrix.npy')
+labels = np.load('labels.npy')
 
-def getTrainBatch():
-    labels = []
+ids, labels = shuffle(ids, labels, random_state = 0)
+lastTrainingIndex = int(round(len(ids) * .90))
+ids = np.split(ids, [lastTrainingIndex, len(ids)])
+labels = np.split(labels, [lastTrainingIndex, len(labels)])
+trainingLabels = labels[0]
+testingLabels = labels[1]
+trainingIds = ids[0]
+testingIds = ids[1]
+
+def getTrainBatch(counter):
+    label = []
     arr = np.zeros([batchSize, maxSeqLength])
     for i in range(batchSize):
-        if (i % 2 == 0): 
-            num = randint(1,3334)
-            labels.append([1,0])
+        arr[i] = trainingIds[counter-1:counter]
+        label.append(trainingLabels[counter])
+        if (counter >= len(trainingIds) - 1):
+            counter = 1
         else:
-            num = randint(4136,5120)
-            labels.append([0,1])
-        arr[i] = ids[num-1:num]
-    return arr, labels
+            counter = counter + 1
+    return arr, label, counter
 
-def getTestBatch():
-    labels = []
+def getTestBatch(counter):
+    label = []
     arr = np.zeros([batchSize, maxSeqLength])
     for i in range(batchSize):
-        num = randint(3335,4135)
-        if (num <= 3734):
-            labels.append([1,0])
+        arr[i] = testingIds[counter-1:counter]
+        label.append(testingLabels[counter])
+        if (counter >= len(testingIds) - 1):
+            counter = 1
         else:
-            labels.append([0,1])
-        arr[i] = ids[num-1:num]
-    return arr, labels
+            counter = counter + 1
+    return arr, label, counter
 
 batchSize = 24
 lstmUnits = 64
 numClasses = 2
-iterations = 100000
+iterations = 10
 
 tf.reset_default_graph()
 
@@ -167,9 +183,10 @@ runlog.write("Batch Size: {}\n".format(batchSize))
 runlog.write("Number of Classes: {}\n".format(numClasses))
 runlog.write("Training Iterations: {}\n".format(iterations))
 
+counter = 1
 for i in range(iterations):
    #Next Batch of reviews
-   nextBatch, nextBatchLabels = getTrainBatch()
+   nextBatch, nextBatchLabels, counter = getTrainBatch(counter)
    sess.run(optimizer, {input_data: nextBatch, labels: nextBatchLabels})
    
    #Write summary to Tensorboard
@@ -185,10 +202,34 @@ writer.close()
 
 saver.restore(sess, tf.train.latest_checkpoint('models'))
 
+counter = 1
+
 iterations = 100
 for i in range(iterations):
-    nextBatch, nextBatchLabels = getTestBatch()
+    nextBatch, nextBatchLabels, counter = getTestBatch(counter)
     print("Accuracy for this batch:", (sess.run(accuracy, {input_data: nextBatch, labels: nextBatchLabels})) * 100)
-    runlog.write("Accuracy for this batch: {}\n".format((sess.run(accuracy, {input_data: nextBatch, labels: nextBatchLabels})) * 100)
+    runlog.write("Accuracy for this batch: {}\n".format((sess.run(accuracy, {input_data: nextBatch, labels: nextBatchLabels})) * 100))
 
+pp = 0
+nn = 0
+npp = 0
+pn = 0
+
+nextBatch, nextBatchLabels, counter = getTestBatch(counter)
+predictions = sess.run(prediction, {input_data: nextBatch, labels: nextBatchLabels})
+for index in range(len(predictions)):
+    positive_sentiment, negative_sentiment = predictions[index][0], predictions[index][1]
+    if (positive_sentiment > negative_sentiment):
+        if ([1,0] == nextBatchLabels[index].tolist()):
+            pp = pp + 1
+        else:
+            npp = npp + 1
+    else:
+        if ([0,1] == nextBatchLabels[index].tolist()):
+            nn = nn + 1
+        else:
+            pn = pn + 1
+
+print(pp, nn)
+                 
 runlog.close()
